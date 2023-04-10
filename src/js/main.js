f1af59d1ee5647a85f20f7cd90917a7502c7655d
Anton Rusinov <13165540+RusinovAnton@users.noreply.github.com>
2023-04-10 23:56:48 +0300

    responsible layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,17 @@
 import "../scss/styles.scss";
+import "bootstrap";
 
 import { InfiniteScroll } from "./infinite-scroll.js";
 import { ShowMore } from "./show-more.js";
 import { ThemeToggle } from "./theme-toggle.js";
 
+const navbar = document.getElementById("navbar-collapsible");
+
 // Easter egg 🤡
 const clickAudio = document.getElementById("click-audio");
 clickAudio.volume = 0.33;
 requestAnimationFrame(() => {
-  [
-    ...[...document.querySelectorAll("a")],
-    ...[...document.querySelectorAll("button")],
-  ].map((el) =>
+  [...document.querySelectorAll("a")].map((el) =>
     el.addEventListener("click", () => {
       clickAudio.play();
     })
