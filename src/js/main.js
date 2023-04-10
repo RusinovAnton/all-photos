cc3ecf0f97461a5c47ebc3cd9186aeceaa26059c
Anton Rusinov <13165540+RusinovAnton@users.noreply.github.com>
2023-04-10 19:08:46 +0300

    fix layout with bs classes

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,17 @@ import "../scss/styles.scss";
 import { InfiniteScroll } from "./infinite-scroll.js";
 import { ShowMore } from "./show-more.js";
 import { ThemeToggle } from "./theme-toggle.js";
+
+// Easter egg 🤡
+const clickAudio = document.getElementById("click-audio");
+clickAudio.volume = 0.33;
+requestAnimationFrame(() => {
+  [
+    ...[...document.querySelectorAll("a")],
+    ...[...document.querySelectorAll("button")],
+  ].map((el) =>
+    el.addEventListener("click", () => {
+      clickAudio.play();
+    })
+  );
+});
