55f310e3391826cca2afccc482875ee2f74d07e1
Anton Rusinov <13165540+RusinovAnton@users.noreply.github.com>
2023-04-11 17:55:42 +0300

    add build folder

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ const path = require("path");
 export default {
   root: path.resolve(__dirname, "src"),
   build: {
-    outDir: "../dist",
+    outDir: "../docs",
   },
+  base: "all-photos",
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
